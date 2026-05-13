5afac03ad8ea3b593cb760ca7e2f484d8e0d26f2
andrefetch <andresilvapersonal2007@gmail.com>
2026-05-13 11:42:42 -0400

    Added inheritance for my parent class, made children classes: savings and checking.

--- main.py
+++ main.py
@@ -18,3 +18,27 @@ class Account:
     
     def get_owner(self):
         return self.owner
+    
+class SavingsAccount(Account):
+    def __init__(self, owner, balance, interest_rate):
+        super().__init__(owner, balance)
+
+        self.interest_rate = interest_rate
+
+    def apply_interest(self):
+        self.interest_rate += (self.interest_rate * 0.25)
+        self.balance += self.interest_rate
+
+class CheckingAccount(Account):
+    def __init__(self, owner, balance, over_draft_limit=1000):
+        super().__init__(owner, balance)
+
+        self.over_draft_limit = over_draft_limit
+    
+    def get_overdraft(self):
+        return self.over_draft_limit
+    
+    def withdraw(self, amount):
+        if amount > self.over_draft_limit:
+            raise Exception(f"Can't withdraw the amount: {amount} because it's over the overdraft limit of: ${self.get_overdraft()}")
+        self.balance -= amount
